4d5545738c28f5cbd3f2b3ea65e7ad0576cfc72b
Masoud Neshastehriz <mneshast@udel.edu>
2025-03-06 17:13:38 -0500

    add Neshastehriz.md

---
+++ students/neshastehriz_masoud/query_results.py
@@ -0,0 +1,73 @@
+"""Top 3 horses in each race:
+
+Race 1:
+             horse  position  ...              jockey                trainer
+3       Insouciant         1  ...  Jevian None Toledo     Brittany T Russell
+4          Amorica         2  ...    Jeremy None Rose  Anthony None Pecoraro
+0  Princess Peanut         3  ...   Xavier None Perez            John J Robb
+
+[3 rows x 5 columns]
+
+Race 2:
+              horse  position  ...                  jockey           trainer
+5          My Jo Jo         1  ...  Alexander None Crispin  Chelsey E Moysey
+0         Satisfied         2  ...           Jean C Alvelo    A Ferris Allen
+3  One Clear Moment         3  ...             Raul E Mena  Michael E Gorham
+
+[3 rows x 5 columns]
+
+Race 3:
+                horse  ...                       trainer
+3  Thtwasthenthisisnw  ...              Michael E Gorham
+0             Keranos  ...              James L Lawrence
+1            Orocovix  ...  Rodolfo None Sanchez-Salomon
+
+[3 rows x 5 columns]
+
+Race 4:
+              horse  position  odds            jockey             trainer
+3   Bring the Magic         1   0.9    Angel S Arroyo    Tim None Woolley
+1            Jaylee         3   4.6  Kevin None Gomez     Keri None Brion
+0  Wine in the Dark         4  12.8  Jeremy None Rose  Richard J Hendriks
+
+Race 5:
+            horse  position  ...                  jockey               trainer
+1      Alva Starr         1  ...        Mychel J Sanchez      Brett A Brinkman
+5  Cheetara (CHI)         2  ...  Vincent None Cheminaud  Ignacio None Correas
+0    Sweet Gracie         3  ...           Jean C Alvelo        A Ferris Allen
+
+[3 rows x 5 columns]
+
+Race 6:
+        horse  position  ...                  jockey                  trainer
+0     Sun Bee         1  ...        Mychel J Sanchez          H Graham Motion
+4     Heckled         2  ...     Florent None Geroux     Michael None Stidham
+1  Anna Lucia         4  ...  Vincent None Cheminaud  Christophe None Clement
+
+[3 rows x 5 columns]
+
+Race 7:
+              horse  position  ...               jockey              trainer
+0      Doppelganger       1.0  ...   Jevian None Toledo   Brittany T Russell
+1        Forewarned       2.0  ...  Dexter None Haddock  Uriah None St Lewis
+2  Ridin With Biden       3.0  ...      Paco None Lopez        Robert E Reid
+
+[3 rows x 5 columns]
+
+Race 8:
+            horse  position  odds               jockey               trainer
+3  Prowling Tiger         1   0.5  Daniel None Centeno  Arnaud None Delacour
+1  Twist 'n Twirl         2   2.9  Florent None Geroux       H Graham Motion
+5       In Effect         3  43.8       Angel S Arroyo         Neil R Morris
+
+Race 9:
+            horse  position  odds                   jockey          trainer
+2       Idiomatic         1   0.4      Florent None Geroux       Brad H Cox
+4  Classy Edition         2   2.5  Kendrick None Carmouche  Todd A Pletcher
+5  Morning Matcha         3   5.9          Paco None Lopez    Robert E Reid
+
+Race 10:
+        horse  position  odds                jockey            trainer
+3   Dialherup       1.0   0.9       Paco None Lopez  Robert None Mosco
+2  Mo Traffic       2.0   1.8      Mychel J Sanchez     Diane D Morici
+1    Kilmaley       3.0   5.5  Jaime None Rodriguez     Michael V Pino"""
